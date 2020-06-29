a27becae9a76f7d4f8d31bf0fb365dfe65915b51
Lingzhe <1119538589@qq.com>
2020-06-29 21:28:10 +0800

    add startApplication

---
+++ src/main/java/cn/bupt/sse/nmp/NationalMuseumSystemApplication.java
@@ -0,0 +1,12 @@
+package cn.bupt.sse.nmp;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalMuseumSystemApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
