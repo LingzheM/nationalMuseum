0beeb3abbfcf8bf72f30082dec5d88b3c4c7153f
lijiaxin <1763634254@qq.com>
2020-06-30 17:05:53 +0800

    mybatis-generator生成entity dao mapper.xml

--- src/main/java/cn/bupt/sse/nmp/NationalMuseumSystemApplication.java
+++ src/main/java/cn/bupt/sse/nmp/NationalMuseumSystemApplication.java
@@ -1,12 +1,15 @@
 package cn.bupt.sse.nmp;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "cn.bupt.sse.nmp")
+@EntityScan(value = "cn.bupt.sse.nmp.entity")
 public class NationalMuseumSystemApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(NationalMuseumSystemApplication.class);
     }
 }
